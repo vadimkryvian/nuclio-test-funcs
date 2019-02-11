c9f2fa814af4639792131eb60cd75bf7801af225
Lev <levr@iguaz.io>
2019-02-11 18:59:50 +0200

    add env to the test

--- py/func_with_external_dep/handler.py
+++ py/func_with_external_dep/handler.py
@@ -1,4 +1,7 @@
 import inflection
+import os
 
 def handler(context, event):
+    if os.getenv('TEST') != 'SUCCESS':
+        raise Exception('Missing env variable TEST')
     return inflection.camelize(event.body.decode('utf-8').strip())
