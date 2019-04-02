ba4492c1c3e402eba896619a9d80828a357b1451
Vadim Kryvian <vadimk@iguazio.com>
2019-04-02 14:17:05 +0300

    WIP

--- py/two_files_func/handler.py
+++ py/two_files_func/handler.py
@@ -2,5 +2,5 @@ import module_a
 
 
 def handler(context, event):
-    get_sum = module_a.TestClass(event).get_sum
+    get_sum = module_a.TestClass(event).get_sum()
     return context.Response(body=str(get_sum), headers=None, content_type='text/plain', status_code=200)
