150cfbd1019662c2867883a0f9b16ead35869fce
Vadim Kryvian <vadimk@iguazio.com>
2019-04-02 14:18:21 +0300

    WIP

--- py/two_files_func/module_a.py
+++ py/two_files_func/module_a.py
@@ -1,7 +1,6 @@
-class TestClass(object):
+class TestClass:
     def __init__(self, event):
         self.nums = event.body.values()
 
-    @property
     def get_sum(self):
         return sum(self.nums)
