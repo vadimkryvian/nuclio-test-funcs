8cb8487815964d8d0409e057096f44405e32c081
Vadim Kryvian <vadimk@iguazio.com>
2019-04-02 14:14:36 +0300

    module rename

--- py/two_files_func/handler.py
+++ py/two_files_func/handler.py
@@ -1,6 +1,6 @@
-import module
+import module_a
 
 
 def handler(context, event):
-    get_sum = module.TestClass(event).get_sum
+    get_sum = module_a.TestClass(event).get_sum
     return context.Response(body=str(get_sum), headers=None, content_type='text/plain', status_code=200)
